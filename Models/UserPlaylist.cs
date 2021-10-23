3216c0220074c22ba26e91064280696b98bc8365
Peiferinc <peiferjustin96@gmail.com>
2021-10-23 15:09:17 -0500

    Initial commit

---
+++ Models/UserPlaylist.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyPractice1.Models
+{
+    public class MyPlaylist
+    {
+
+        public class UserPlaylist
+        {
+            public string href { get; set; }
+            public Item[] items { get; set; }
+            public int limit { get; set; }
+            public string next { get; set; }
+            public int offset { get; set; }
+            public object previous { get; set; }
+            public int total { get; set; }
+        }
+
+        public class Item
+        {
+            public bool collaborative { get; set; }
+            public string description { get; set; }
+            public External_Urls external_urls { get; set; }
+            public string href { get; set; }
+            public string id { get; set; }
+            public object[] images { get; set; }
+            public string name { get; set; }
+            public Owner owner { get; set; }
+            public object primary_color { get; set; }
+            public bool _public { get; set; }
+            public string snapshot_id { get; set; }
+            public Tracks tracks { get; set; }
+            public string type { get; set; }
+            public string uri { get; set; }
+        }
+
+        public class External_Urls
+        {
+            public string spotify { get; set; }
+        }
+
+        public class Owner
+        {
+            public string display_name { get; set; }
+            public External_Urls1 external_urls { get; set; }
+            public string href { get; set; }
+            public string id { get; set; }
+            public string type { get; set; }
+            public string uri { get; set; }
+        }
+
+        public class External_Urls1
+        {
+            public string spotify { get; set; }
+        }
+
+        public class Tracks
+        {
+            public string href { get; set; }
+            public int total { get; set; }
+        }
+
+    }
+}
---
+++ MyServices/ISpotifyService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyServices
+{
+    public interface ISpotifyService
+    {
+        Task<IEnumerable<Release>> GetNewReleases(string countryCode, int limit, string accessToken);
+    }
+}
